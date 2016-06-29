02f5495db9670702d6e34884774628444a21f8bd
Francesco Gazzetta <fgaz@users.noreply.github.com>
2016-06-29 18:22:43 +0200

    newline on command

--- pwm.py
+++ pwm.py
@@ -1,7 +1,7 @@
 def set(pin, value):
     try:
         f = open("/dev/pi-blaster", 'w')
-        f.write(str(pin) + "=" + str(value))
+        f.write(str(pin) + "=" + str(value) + "\n")
         f.close()	
     except:
         print("Error writing to: " + property + " value: " + value)
